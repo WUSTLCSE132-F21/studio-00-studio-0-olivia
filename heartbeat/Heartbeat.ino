5f68e580b79f4a30305f62fd0079c32f2d61201e
OliviaWang233 <64058608+OliviaWang233@users.noreply.github.com>
2021-08-30 18:26:01 -0500

    heartbeat

--- heartbeat/Heartbeat.ino
+++ heartbeat/Heartbeat.ino
@@ -1,9 +1,17 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int time = millis()/1000;
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(500);
+  Serial.print(time);
+  Serial.println(" sec have elapsed");
 
+  
 }
